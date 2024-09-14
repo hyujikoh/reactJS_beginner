c446980d6daf481442079c9e7665e03f2e44f8f2
hyujikoh <dvum0045@gmail.com>
2024-09-14 14:47:32 +0900

    cra

---
+++ create_react/hyujik-react/src/Button.js
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types"
+import styles from "./Button.module.css"
+
+function Button({text}){
+    return <button className={styles.btn}>{text}</button>
+}
+
+
+
+Button.prototypes = {
+    text : PropTypes.string.isRequired,
+}
+
+export default Button
